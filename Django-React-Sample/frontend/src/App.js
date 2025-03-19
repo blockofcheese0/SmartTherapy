d7d15a7f6059ba72176cbc665228daf19552f990
Emma Zhou <yiyuanemmazhou@macbookair.lan>
2025-03-19 18:41:58 -0400

    Copy from divergent-teams-hack

---
+++ Django-React-Sample/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Smart Therapy</h1>
+    <p>Hello</p>
+   </div>
+  );
+}
+
+export default App;
